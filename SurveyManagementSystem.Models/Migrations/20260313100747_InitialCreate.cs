0490eb6b96c608b2ed2e5571ccc93adfe967d3f0
DawnHavoc <sarnavadas2@gmail.com>
2026-03-13 16:48:02 +0530

    Created Repositories, Migrations

---
+++ SurveyManagementSystem.Models/Migrations/20260313100747_InitialCreate.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SurveyManagementSystem.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Role",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Role", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Phone = table.Column<int>(type: "int", nullable: false),
+                    RoleId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_User_Role_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Role",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_RoleId",
+                table: "User",
+                column: "RoleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "Role");
+        }
+    }
+}
---
+++ SurveyManagementSystem.Models/Repositories/UserRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SurveyManagementSystem.BLL.DTOs;
+using SurveyManagementSystem.BLL.Entities;
+using SurveyManagementSystem.DAL.Data;
+
+
+namespace SurveyManagementSystem.DAL.Repositories
+{
+    public class UserRepository : IUser
+    {
+        private readonly ApplicationDbContext? _Dbcontext;
+        public UserRepository(ApplicationDbContext _Dbcontext)
+        {
+            _Dbcontext = _Dbcontext;
+        }
+
+        public async Task<ServiceRespone> CreateAsync(User user)
+        {
+            //throw new NotImplementedException();
+            var check = await _Dbcontext.User.FirstOrDefaultAsync(u => u.Name.ToLower() == user.Name.ToLower());
+            if(check != null)
+                return new ServiceRespone(false, "User already exists");
+
+            await _Dbcontext.AddAsync(user);
+            await _Dbcontext.SaveChangesAsync();
+            return new ServiceRespone(true, "User added successfully");
+        }
+        public async Task<ServiceRespone> UpdateAsync(User user)
+        {
+            
+
+            _Dbcontext.Update(user);
+            await _Dbcontext.SaveChangesAsync();
+            return new ServiceRespone(true, "User update successfully");
+        }
+      
+        public async Task<ServiceRespone> DeleteAsync(int id)
+        {
+            var user = await _Dbcontext.User.FindAsync(id);
+            if (user == null)
+                return new ServiceRespone(false, "User does not exist");
+           
+            _Dbcontext.Remove(user);
+            await _Dbcontext.SaveChangesAsync();
+            return new ServiceRespone(true, "User deleted successfully");
+        }
+
+        public async Task<List<User>> GetAsync()
+        {
+            return await _Dbcontext.User.AsNoTracking().ToListAsync();
+        }
+      
+        public async Task<User> GetByIdAsync(int id)=>await _Dbcontext.User.FindAsync(id);
+       
+    }
+}
