fac49acd3c05dafcba63a6ff348618581cec192b
DawnHavoc <sarnavadas2@gmail.com>
2026-03-13 16:48:40 +0530

    Added Controllers

---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SurveyManagementSystem.BLL.Entities;
+using SurveyManagementSystem.DAL.Repositories;
+
+namespace WebAPI.Controllers
+{
+    public class UserController : ControllerBase
+    {
+        private readonly IUser _user;
+
+        public UserController(IUser user)
+        {
+            this._user = user;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] User userDTO)
+        {
+            var result = await _user.CreateAsync(userDTO);
+            return Ok(result);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update([FromBody] User userDTO)
+        {
+            var result = await _user.UpdateAsync(userDTO);
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _user.DeleteAsync(id);
+            return Ok(result);
+        }
+
+        public async Task<IActionResult> Get()
+        {
+            var data = await _user.GetAsync();
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var data = await _user.GetByIdAsync(id);
+            return Ok(data);
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using SurveyManagementSystem.DAL.Data;
+using SurveyManagementSystem.DAL.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//builder.Services.AddTransient<IUser, UserRepository>();
+builder.Services.AddScoped<IUser, UserRepository>();
 
 var app = builder.Build();
 
