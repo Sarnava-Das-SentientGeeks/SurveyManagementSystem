be83fa909f75f0f520325cd930bbb785c5db9729
DawnHavoc <sarnavadas2@gmail.com>
2026-03-13 16:59:47 +0530

    Added Razor pages for UI

---
+++ SMSWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SMSWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
